164633e367047bc93114b838e86b2b35173a40a1
ipxe <admin@d1ablo.ru>
2015-11-29 02:13:04 +0300

    v0.2

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 ./gradlew clean && ./gradlew build
+
+cp -f build/libs/*.jar package/opt/myapp/
+fpm -s dir -t rpm -n myapp -v 0.1 -d java-1.6.0-openjdk --post-install package/postinstall --pre-uninstall package/preuninstall -C package/
