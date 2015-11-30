9e77ef6afa91dac794cbce042613af8eb91561ea
root <admin@d1abrlo.ru>
2015-11-30 14:09:46 +0300

    v1.0

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-./gradlew clean && ./gradlew build
-
-cp -f build/libs/*.jar package/opt/myapp/myapp.jar
-fpm -s dir -t rpm -n myapp -v 0.1 -d java-1.6.0-openjdk --post-install package/postinstall --pre-uninstall package/preuninstall -C package/
+./gradlew clean && ./gradlew build && ./gradlew buildRpm
